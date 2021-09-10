12fdd7d0dbdce1b03208359e85d5963824f96608
Vinay288 <vinayhiremath288@gmail.com>
2021-09-10 19:06:53 +0530

    REFACTORED: constructors to initialize message

--- main/java/MoodAnalyser/MoodAnalyser.java
+++ main/java/MoodAnalyser/MoodAnalyser.java
@@ -3,15 +3,21 @@ package MoodAnalyser;
 import MoodAnalyser.MoodAnalysisException.ExceptionType;
 
 public class MoodAnalyser {
+	private String message;
 
-	
-	public String analyseMood(String message) {
-		
-			
-			if (message.contains("sad"))
-				return "SAD";
-			else
-				return "HAPPY";
-		
+public MoodAnalyser() {
+	this.message="sad";
+}
+
+	public MoodAnalyser(String message) {
+		this.message = message;
+	}
+
+	public String analyseMood() {
+		if (this.message.contains("sad"))
+			return "SAD";
+		else
+			return "HAPPY";
+
 	}
 }
