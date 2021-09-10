6ffbe2b09d08cefaaceb0029bce57bf021915647
Vinay288 <vinayhiremath288@gmail.com>
2021-09-10 18:57:37 +0530

    ADDED: method to analyse mood

---
+++ main/java/MoodAnalyser/MoodAnalyser.java
@@ -0,0 +1,17 @@
+package MoodAnalyser;
+
+import MoodAnalyser.MoodAnalysisException.ExceptionType;
+
+public class MoodAnalyser {
+
+	
+	public String analyseMood(String message) {
+		
+			
+			if (message.contains("sad"))
+				return "SAD";
+			else
+				return "HAPPY";
+		
+	}
+}
