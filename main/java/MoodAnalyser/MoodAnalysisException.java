ed6954149b9f3f13687b8ca45220ff542e4d2c15
Vinay288 <vinayhiremath288@gmail.com>
2021-09-10 20:00:23 +0530

    ADDED: custom exception for mood analysis with enum

---
+++ main/java/MoodAnalyser/MoodAnalysisException.java
@@ -0,0 +1,13 @@
+package MoodAnalyser;
+
+public class MoodAnalysisException extends Exception {
+
+	enum ExceptionType{
+		ENTERED_NULL,ENTERED_EMPTY
+	}
+	ExceptionType type;
+	public MoodAnalysisException(ExceptionType type,String message) {
+		super(message);
+		this.type=type;
+	}
+}
