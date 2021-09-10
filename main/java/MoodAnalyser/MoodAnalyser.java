2192cb11c5a6890e169c45e8b4f3b1718df6d6ac
Vinay288 <vinayhiremath288@gmail.com>
2021-09-10 19:57:57 +0530

    ADDED: try catch block for anlyse method to catch nullpointer exception

--- main/java/MoodAnalyser/MoodAnalyser.java
+++ main/java/MoodAnalyser/MoodAnalyser.java
@@ -5,19 +5,24 @@ import MoodAnalyser.MoodAnalysisException.ExceptionType;
 public class MoodAnalyser {
 	private String message;
 
-public MoodAnalyser() {
-	this.message="sad";
-}
+	public MoodAnalyser() {
+		this.message = "sad";
+	}
 
 	public MoodAnalyser(String message) {
 		this.message = message;
 	}
 
 	public String analyseMood() {
+		try {
 		if (this.message.contains("sad"))
 			return "SAD";
 		else
 			return "HAPPY";
+		}
+		catch(NullPointerException e) {
+			return "HAPPY";
+		}
 
 	}
 }
